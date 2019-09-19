b86b3cf1c60afb5edb4218c338fd93d262232f6b
Giorgi <gio@neollet.com>
2019-09-19 19:15:53 +0400

    Added class libraries for modularity

--- Balena.IOT.RealTimeMonitor.Api/Controllers/ValuesController.cs
+++ Balena.IOT.RealTimeMonitor.Api/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Balena.IOT.RealTimeMonitor.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
---
+++ Belane.IOT.Simulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Belane.IOT.Simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: simulate UFO-s :) 
+        }
+    }
+}
